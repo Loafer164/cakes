15eb62a93f87cd8f8048de46ee332d4cc842e093
Loafer164 <Loafer164@gmail.com>
2020-02-26 11:03:46 +0300

    1
    1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const modalWrapper = document.getElementById('modal-wrapper')
+const close = document.getElementById('x')
+const modalCookie = document.getElementById('modal-cookie')
+
+function openModal() {
+    modalWrapper.style.display='block'
+}
+
+function closeModal() {
+    modalWrapper.style.display='none'
+}
+
+function closeModalCookie() {
+    modalCookie.style.display='none'
+}
